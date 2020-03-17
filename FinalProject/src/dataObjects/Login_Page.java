1d9ef7529c617eb981cf6a06b987749c622eb514
pterodactella <daniela.bahneanu@gmail.com>
2020-03-17 10:29:08 +0100

    p1

--- FinalProject/src/dataObjects/Login_Page.java
+++ FinalProject/src/dataObjects/Login_Page.java
@@ -1,5 +1,26 @@
 package dataObjects;
 
-public class Login_Page {
+import java.util.Scanner;
 
+public class Login_Page {
+	// the path takes the value of the file context where all the data is
+	// I assume we will search for username and password are searched for in the file
+    private static Scanner x;
+	public void get_input(String username, String password, String path) {
+		boolean searching = false;
+		String templogin = "";
+		String tempPass = "";
+		
+		try {
+			x = new Scanner (new File(path));
+			
+			
+		}
+		
+		catch(Exception e) {
+			
+		}
+		
+		
+		}
 }
