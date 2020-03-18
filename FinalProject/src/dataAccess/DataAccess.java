d4270ff515c347fef5d3eb038a886706f04eb336
Barrikad <s184459@student.dtu.dk>
2020-03-18 09:28:05 +0100

    Finished xsd structure. Created DataAccess class

---
+++ FinalProject/src/dataAccess/DataAccess.java
@@ -0,0 +1,20 @@
+package dataAccess;
+import java.io.*;
+import java.util.*;
+import javax.xml.stream.*;
+import javax.xml.stream.events.*;
+
+public class DataAccess {
+	File dataBase;
+	public void newEntry() {
+		
+	}
+	
+	public void editEntry() {
+		
+	}
+	
+	public String getEntry(ID entryID) {
+		
+	}
+}
