e10a2670050ccd847ede45c1ab950a832c39dcb5
Mamuna <muna.azam@hotmail.com>
2020-03-22 13:04:02 +0100

    Created classes for the unit tests for objectsdata

---
+++ FinalProject/src/objectsDataTest/ReferenceNameTest.java
@@ -0,0 +1,14 @@
+package objectsDataTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReferenceNameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
