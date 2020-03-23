cf2fc0afca6a452cc4583bbd09d59ff8e78ea84e
Barrikad <s184459@student.dtu.dk>
2020-03-23 11:00:07 +0100

    Implemented create client entry

--- FinalProject/src/dataAccess/ClientAccess.java
+++ FinalProject/src/dataAccess/ClientAccess.java
@@ -2,25 +2,52 @@ package dataAccess;
 
 import objectsData.ClientData;
 
+
 import java.io.*;
 import javax.xml.parsers.*;
 import javax.xml.validation.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
 import org.w3c.dom.*;
 
 
 public class ClientAccess extends DataAccess<ClientData> {
-
+	
+	
 	ClientAccess() {
 		super("storage/clients.xml");
 	}
 	
 	public void newEntry(ClientData data) {
 		Element root = doc.getDocumentElement();
+		
+		Element newClient = createNewClientElement(data);
+		
 		NodeList clients = root.getChildNodes();
+		int clientsLength = clients.getLength();
+		int clientIDValue = data.getClientId();
 		
-		Element newClient = doc.createElement("Client");
+		if( (clientsLength == 0) || (clientIDValue >= getElementID(clients.item(clientsLength - 1)))) {
+			root.appendChild(newClient);
+		}
+		else {
+			insertElementSorted(root, newClient);
+		}
 		
+		try {
+			
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(new File(filePath));
+		transformer.transform(source, result);
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
+
 
 	public void editEntry(ClientData data) {
 		
@@ -30,4 +57,36 @@ public class ClientAccess extends DataAccess<ClientData> {
 		return null;
 	}
 
+	
+	public Element createNewClientElement(ClientData data) {
+		Element newClient = doc.createElement("Client");
+		newElementWithValue(newClient, "ClientID", String.valueOf(data.getClientId()));
+		newElementWithValue(newClient, "CompanyName", data.getCompanyName());
+		newElementWithValue(newClient, "PhoneNumber", String.valueOf(data.getPhoneNumber()));
+		newElementWithValue(newClient, "Email", data.getEmail());
+		
+		Name name = data.getName();
+		Element nameElement = doc.createElement("Name");
+		newElementWithValue(nameElement,"FirstName",name.getFirstName());
+		for(String middleName : name.getMiddleName()) {
+			newElementWithValue(nameElement,"MiddleName",middleName);
+		}
+		newElementWithValue(nameElement,"LastName",name.getLastName());
+		newClient.appendChild(nameElement);
+		
+		Address address = data.getAddress();
+		Element addressElement = doc.createElement("Address");
+		newElementWithValue(addressElement,"StreetName",address.getStreetName());
+		newElementWithValue(addressElement,"HouseNumber",String.valueOf(address.getHouseNumber()));
+		newElementWithValue(addressElement,"City",address.getCity());
+		newElementWithValue(addressElement,"ZipCode",String.valueOf(address.getZipCode));
+		newClient.appendChild(addressElement);
+		
+		Element activeShipments = doc.createElement("ActiveShipments");
+		for(long journeyID : data.getActiveShipments()) {
+			newElementWithValue(activeShipments,"JourneyID",String.valueOf(journeyID));
+		}
+		newClient.appendChild(activeShipments);
+		return newClient;
+	}
 }
--- FinalProject/src/dataAccess/DataAccess.java
+++ FinalProject/src/dataAccess/DataAccess.java
@@ -1,5 +1,6 @@
 package dataAccess;
 
+import objectsData.ClientData;
 import objectsData.ObjectData;
 
 import java.io.*;
@@ -8,25 +9,31 @@ import javax.xml.validation.*;
 import org.w3c.dom.*;
 
 public class DataAccess<T extends ObjectData> {
+	String filePath;
 	File dataBase;
 	File schemaFile;
 	Document doc;
 	Schema schema;
 	
+	
 	DataAccess(String fileName) {
+		filePath = fileName;
+		
 		try {
-			SchemaFactory schemaFactory = SchemaFactory.newDefaultInstance();
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			
-			dataBase = new File(fileName);
-			schemaFile = new File("storage/dataStructure.xsd");
-			
-			schema = schemaFactory.newSchema(schemaFile);
-			dbFactory.setSchema(schema);
-			
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			doc = dBuilder.parse(dataBase);
-			doc.getDocumentElement().normalize();
+		SchemaFactory schemaFactory = SchemaFactory.newDefaultInstance();
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		
+		dataBase = new File(filePath);
+		schemaFile = new File("storage/dataStructure.xsd");
+		
+		schema = schemaFactory.newSchema(schemaFile);
+		dbFactory.setSchema(schema);
+		
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		doc = dBuilder.parse(dataBase);
+		doc.getDocumentElement().normalize();
+		
 		}
 		catch (Exception e) {
 			e.printStackTrace();
@@ -43,5 +50,44 @@ public class DataAccess<T extends ObjectData> {
 	
 	public T getEntry(int ID) {
 		return null;
+	}
+	
+	public void newElementWithValue(Element parentElement, String elementName, String elementValue) {
+		Element element = doc.createElement(elementName);
+		Text valueNode = doc.createTextNode(String.valueOf(elementValue));
+		
+		element.appendChild(valueNode);
+		parentElement.appendChild(element);
+	}
+	
+	public int getElementID(Node node) {
+		return Integer.parseInt(node.getChildNodes().item(0).getTextContent());
+	}
+	
+	public void insertElementSorted(Element root, Element newClient) {
+		int newClientIDValue = getElementID(newClient);
+		NodeList clients = root.getChildNodes();
+		int lower = 0;
+		int upper = clients.getLength() - 1;
+		int index;
+		int curID;
+		
+		do {
+			index = (lower + upper)/2;
+			
+			curID = getElementID(clients.item(index));
+			if(curID > newClientIDValue) {
+				lower = index;
+			}
+			else if(curID < newClientIDValue) {
+				upper = index;
+			}
+			else {
+				upper = index;
+				lower = index;
+			}
+		} while (Math.abs(lower - upper) >= 1);
+		
+		root.insertBefore(newClient, clients.item(upper));
 	}
 }
