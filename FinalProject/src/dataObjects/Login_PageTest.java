24fd4d489892e7eadfad6ca5d9d32444f4cecdfe
pterodactella <daniela.bahneanu@gmail.com>
2020-03-23 20:53:39 +0100

    Update Login_PageTest.java

--- FinalProject/src/dataObjects/Login_PageTest.java
+++ FinalProject/src/dataObjects/Login_PageTest.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class Login_PageTest {
-
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
+		
 	}
 
 }
