706988a9d11da6517e56900a39a5f06b097004e6
pterodactella <daniela.bahneanu@gmail.com>
2020-03-23 21:23:46 +0100

    Update Login_PageTest.java

--- FinalProject/src/dataObjects/Login_PageTest.java
+++ FinalProject/src/dataObjects/Login_PageTest.java
@@ -1,18 +1,32 @@
 package dataObjects;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-class Login_PageTest {
-	
-	@Test
-	void testget_input() {
-		
-		fail("Not yet implemented");
-		
-		
-	}
-
-
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.BaseMatcher.*;
+import java.util.ArrayList;
+import java.util.List;
+ 
+import org.junit.Before;
+import org.junit.Test;
+ 
+public class Login_PageTest {
+     
+	boolean found = false;
+	String templogin = "";
+	String tempPass = "";;
+     
+    @Before
+    public void setData(){
+        this.templogin = "client1";
+        this.tempPass = "admin";
+        ArrayList<String> listOfValidStrings = new ArrayList<String>();
+        listOfValidStrings.add("uchvbsh");
+        listOfValidStrings.add("vbsu");
+    }
+     
+    @Test
+    public void testget_Input() {
+        assertThat("admin",is("client1"));
+    }
+    
+//how to properly make it because it's badddddddd
 }
