bc37991bcd4784d00e5b6ec06331b8d129dbbbaa
Katherine Fernandes <39100068+katherinefernandes@users.noreply.github.com>
2020-03-24 21:19:30 +0100

    validate phone number
    format +xxxxxxxxxxxxx or xxxxxxxxxxxxxx

--- FinalProject/src/dataObjects/Testing.java
+++ FinalProject/src/dataObjects/Testing.java
@@ -4,7 +4,7 @@ public class Testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println( validInput.validateEmail("Katherine.fernan55desrj@company.dk"));
+		System.out.println( validInput.validatePhone("447911123456"));
 	}
 
 }
--- FinalProject/src/dataObjects/validInput.java
+++ FinalProject/src/dataObjects/validInput.java
@@ -9,5 +9,8 @@ public class validInput {
 		return email.matches("^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\\.[a-zA-Z]{2,4}");
 	}
 	
+	public static boolean validatePhone (String phone) {
+		return phone.matches("^[+]{0,1}[0-9]{8,15}");
+	}
 
 }
