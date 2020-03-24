cb788775d573042f70d832e0a64466d864e8b5bb
Mamuna <muna.azam@hotmail.com>
2020-03-24 10:19:40 +0100

    updated the positioning of the files

--- FinalProject/src/supportingClasses/Security.java
+++ FinalProject/src/supportingClasses/Security.java
@@ -1,33 +1,39 @@
 package supportingClasses;
 
-
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 class Security {	
-    public void get_ID() throws FileNotFoundException {
+	public long get_ID() throws FileNotFoundException {
     	//we need to save the id in the txt file
-    	File file = new File("output.txt");
+//    	File file = new File("output.txt");
+//    	FileOutputStream fos = new FileOutputStream(file);
+//    	PrintStream ps = new PrintStream(fos);
+//    	System.setOut(ps);
+		long ID = System.nanoTime();
+		//long journeyID = System.nanoTime();
+		//long containerID = System.nanoTime();
+		//System.out.println("userID "+ userID);
+		//System.out.println("journeyID "+ journeyID);
+		//System.out.println("ID: "+ ID);
+		File file = new File("output.txt");
     	FileOutputStream fos = new FileOutputStream(file);
     	PrintStream ps = new PrintStream(fos);
     	System.setOut(ps);
-		long userID = System.nanoTime();
-		long journeyID = System.nanoTime();
-		long containerID = System.nanoTime();
-		System.out.println("userID "+ userID);
-		System.out.println("journeyID "+ journeyID);
-		System.out.println("containerID "+ containerID);
+		return ID;
 	
-}
+}    
 
 
 //public static void main(String[] args) throws FileNotFoundException {
 //	Security s = new Security();
 //	s.get_ID();
-	
+////
+////	
+////	
+////	
 //}
 
 
---
+++ FinalProject/test/userInterfaceTests/Login_PageTest.java
@@ -0,0 +1,15 @@
+package userInterfaceTests;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Login_PageTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
