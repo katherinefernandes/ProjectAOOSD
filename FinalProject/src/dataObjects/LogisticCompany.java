bbb6e348ac1c700e6fe02386f5a135d28e2202b6
Katherine Fernandes <39100068+katherinefernandes@users.noreply.github.com>
2020-03-24 08:56:07 +0100

    :)

--- FinalProject/src/dataObjects/LogisticCompany.java
+++ FinalProject/src/dataObjects/LogisticCompany.java
@@ -5,7 +5,7 @@ public class LogisticCompany {
 	public void addClient () {
 		Scanner s = new Scanner(System.in);
 		
-		System.out.println("Name: ");
+		System.out.println("Company name: ");
 		String name = s.nextLine();
 		
 		System.out.println("E-mail: ");
@@ -14,15 +14,34 @@ public class LogisticCompany {
 		System.out.println("Phone number: ");
 		String phone = s.nextLine();
 		
-		System.out.println("Reference person: ");
-		String person = s.nextLine();
+		System.out.println("Reference person ");
+		System.out.println("First name: ");
+		String firstName = s.nextLine();
 		
-		System.out.println("Address: ");
-		String address = s.nextLine();
+		System.out.println("Middle name: ");
+		String middleName = s.nextLine();
 		
-		//enter the generate ID
+		System.out.println("Last name: ");
+		String lastName = s.nextLine();
 		
-		accessClient.addData(name, email, phone, address, id);
+		System.out.println("Street: ");
+		String street = s.nextLine();
+		
+		System.out.println("City: ");
+		String city = s.nextLine();
+		
+		System.out.println("Building number: ");
+		String bNumber = s.nextLine();
+		
+		System.out.println("Post code: ");
+		String zip = s.nextLine();
+		
+		long id = Security.generateID();
+		
+		//Check for valid input
+		ClientData newClient = new ClientData(id, name, phone, email, firstName, middleName, lastName, street, city, bNumber, zip);
+		
+		ClientAccess.newEntry(newClient);
 		s.close();
 	}
 	
@@ -31,7 +50,29 @@ public class LogisticCompany {
 	}
 	
 	public void updateContainer() {
+		Scanner s = new Scanner(System.in);
 		
+		System.out.println("ID of the container: ");
+		String idc = s.nextLine();
+		
+		System.out.println("New longitude: ");
+		String lon = s.nextLine();
+		
+		System.out.println("New latitude: ");
+		String lat = s.nextLine();
+		
+		System.out.println("Atmosphere: ");
+		String atm = s.nextLine();
+		
+		System.out.println("Humidity: ");
+		String hum = s.nextLine();
+		
+		System.out.println("Temperature: ");
+		String temp = s.nextLine();
+		
+		
+		
+		s.close();
 	}
 	
 	public void getInfoContainer() {
---
+++ FinalProject/src/dataObjects/validInput.java
@@ -0,0 +1,9 @@
+package dataObjects;
+
+public class validInput {
+	public <T> boolean validateInput(T data) {
+		if (data instanceof ClientData) {
+			
+		}
+	}
+}
