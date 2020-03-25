598c7bea797878dd891203421f4938e49c71dcc4
Mamuna <muna.azam@hotmail.com>
2020-03-25 09:00:22 +0100

    completed the objectData tests

--- FinalProject/test/objectsDataTest/ContainerDataTest.java
+++ FinalProject/test/objectsDataTest/ContainerDataTest.java
@@ -29,73 +29,66 @@ public class ContainerDataTest {
 	public void testContainerData() {
 		
 		objectTest = new ContainerData( cid,clid, jid, spid,  dpid,  lat,  lon,  cargo,  t,  a,  h, arriveby);
-		
+	 
 		
 	}
-
+	
 	@Test
 	public void testUseContainerAgain() {
-		fail("Not yet implemented");
+		assertSame(cargo,objectTest.getCargo());
+		objectTest.useContainerAgain(clid, jid, spid,  dpid,  lat,  lon,  "apple",  t,  a,  h, arriveby);
+		assertSame("apple",objectTest.getCargo());
 	}
 
 	@Test
 	public void testSetCurrentPosition() {
-		fail("Not yet implemented");
+			assertEquals((int)lat,(int)objectTest.getCurrentPosition().getLatitude());
+			objectTest.setCurrentPosition(34.0f, 36.9f);
+			assertEquals((int)34.0f,(int)objectTest.getCurrentPosition().getLatitude());
 	}
 
 	@Test
 	public void testSetStatus() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetCurrentPosition() {
-		fail("Not yet implemented");
+		assertEquals((int)t,(int)objectTest.getInternalStatus().getTemperature());
+		objectTest.setStatus(a, 32.0f, h);
+		assertEquals((int)32.0f,(int)objectTest.getInternalStatus().getTemperature());
+	
 	}
 
 	@Test
 	public void testGetUpdated() {
-		fail("Not yet implemented");
+		assertNotSame(arriveby,objectTest.getUpdated());
+		
 	}
 
 	@Test
 	public void testGetArriveBy() {
-		fail("Not yet implemented");
+		assertSame(arriveby,objectTest.getArriveBy());
 	}
 
 	@Test
 	public void testGetContainerID() {
-		fail("Not yet implemented");
+		assertEquals(cid,objectTest.getContainerID());
 	}
 
 	@Test
 	public void testGetJourneyID() {
-		fail("Not yet implemented");
+		assertEquals(jid,objectTest.getJourneyID());
 	}
 
 	@Test
 	public void testGetClientID() {
-		fail("Not yet implemented");
+		assertEquals(clid,objectTest.getClientID());
 	}
 
 	@Test
 	public void testGetDestinationPortID() {
-		fail("Not yet implemented");
+		assertEquals(dpid,objectTest.getDestinationPortID());
 	}
 
 	@Test
 	public void testGetStartPortID() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetCargo() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetInternalStatus() {
-		fail("Not yet implemented");
+		assertEquals(spid,objectTest.getStartPortID());
 	}
 
 }
