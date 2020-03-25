be2cf2deae0e5a4e080fef91526ce8fb1b717d8e
Barrikad <s184459@student.dtu.dk>
2020-03-25 14:12:00 +0100

    O LORD THE CONFLICTS

--- FinalProject/src/dataAccess/DataAccess.java
+++ FinalProject/src/dataAccess/DataAccess.java
@@ -4,6 +4,11 @@ import objectsData.ObjectData;
 
 import java.io.*;
 import javax.xml.parsers.*;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.validation.*;
 import org.w3c.dom.*;
 
@@ -43,11 +48,11 @@ public class DataAccess<T extends ObjectData> {
 		
 	}
 	
-	public void editEntry(T data) {
+	public void editEntry(T data) throws ElementNotFoundException {
 		
 	}
 	
-	public T getEntry(long ID) {
+	public T getEntry(long ID) throws ElementNotFoundException {
 		return null;
 	}
 	
@@ -61,34 +66,94 @@ public class DataAccess<T extends ObjectData> {
 		parentElement.appendChild(element);
 	}
 	
-	protected int getElementID(Node node) {
-		return Integer.parseInt(node.getChildNodes().item(0).getTextContent());
+	protected int getElementID(Element element) {
+		return Integer.parseInt(element.getChildNodes().item(0).getTextContent());
 	}
 	
-	protected void insertElementSorted(Element root, Element newClient) {
-		int newClientIDValue = getElementID(newClient);
-		NodeList clients = root.getChildNodes();
+	protected void insertElement(Element newElement) {
+		Element root = doc.getDocumentElement();
+		long newElementID = getElementID(newElement);
+		NodeList elements = root.getChildNodes();
+		int elementsLen = elements.getLength();
+		int insertionIndex;
+		
+		if(elementsLen > 0) {
+			insertionIndex = searchSupremum(elements,newElementID);
+		}
+		else {
+			insertionIndex = 1;
+		}
+		
+		if (insertionIndex < elementsLen) {
+			root.insertBefore(newElement, elements.item(insertionIndex));
+		}
+		else {
+			root.appendChild(newElement);
+		}
+	}
+	
+	protected Element getElementFromID(long ID) throws ElementNotFoundException {
+		Element root = doc.getDocumentElement();
+		NodeList nodes = root.getChildNodes();
+		int nodesLen = nodes.getLength();
+		Node closestNode;
+		
+		if (nodesLen == 0 || getElementID((Element) (closestNode = nodes.item(searchSupremum(nodes, ID)))) != ID) {
+			throw new ElementNotFoundException("Element with given ID not found");
+		}
+
+		return (Element) closestNode;
+	}
+	
+	protected void transform() throws TransformerFactoryConfigurationError {
+		try {
+			
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(new File(filePath));
+		transformer.transform(source, result);
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected Element elementFromData(T data) {
+		return null;
+	}
+	
+	protected T dataFromElement(Element element) {
+		return null;
+	}
+	//--------HELPER HELPER METHODS(private visibility)---------
+	private int searchSupremum(NodeList nodes, long ID) {
 		int lower = 0;
-		int upper = clients.getLength() - 1;
+		int upper = nodes.getLength() - 1;
 		int index;
-		int curID;
+		long curID;
 		
 		do {
 			index = (lower + upper)/2;
 			
-			curID = getElementID(clients.item(index));
-			if(curID > newClientIDValue) {
-				lower = index;
+			curID = getElementID((Element) nodes.item(index));
+			if(curID < ID) {
+				lower = index + 1;
 			}
-			else if(curID < newClientIDValue) {
-				upper = index;
+			else if(curID > ID) {
+				upper = index - 1;
 			}
 			else {
 				upper = index;
 				lower = index;
 			}
-		} while (Math.abs(lower - upper) >= 1);
+		} while (lower < upper);
 		
-		root.insertBefore(newClient, clients.item(upper));
+		if (index == -1) {
+			return 0;
+		}if (ID > curID) {
+			return index + 1;
+		}
+		return index;
 	}
 }
