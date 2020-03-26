6400b014746f278cfcb9d2197a5846ee27c34492
Barrikad <s184459@student.dtu.dk>
2020-03-26 12:39:56 +0100

    Removed dataccesstest. Refactored clientaccess

--- FinalProject/src/dataAccess/ClientAccess.java
+++ FinalProject/src/dataAccess/ClientAccess.java
@@ -29,20 +29,18 @@ public class ClientAccess extends DataAccess<ClientData> {
 		Element newClient = elementFromData(data);
 		
 		NodeList clients = root.getChildNodes();
-		int clientsLength = clients.getLength();
 		long clientIDValue = data.getClientID();
 		
-		if( (clientsLength == 0) || (clientIDValue >= nodeMethods.getElementID((Element) clients.item(clientsLength - 1)))) {
+		if( nodeMethods.needsToBeInsertedAtEnd(clients, clientIDValue)) {
 			root.appendChild(newClient);
 		}
 		else {
 			insertElement(newClient);
 		}
 		
-		
 		transform();
 	}
-
+	
 
 	public void editEntry(ClientData data) throws ElementNotFoundException{
 		Element root = doc.getDocumentElement();
@@ -55,33 +53,34 @@ public class ClientAccess extends DataAccess<ClientData> {
 		transform();
 	}
 
+	
 	public ClientData getEntry(long ID) throws ElementNotFoundException, NumberFormatException, AmbiguousElementSelectionException {
 		Element client = getElementFromID(ID);
 		ClientData clientData = dataFromElement(client);
 		return clientData;
 	}
-
-	
 	
 	
 	protected Element elementFromData(ClientData data) {
 		Element newClient = doc.createElement("Client");
+		Element phoneNumber = doc.createElement("PhoneNumber");
+		ReferenceName name = data.getPerson();
+		Element nameElement = doc.createElement("RefrencePersonName");
+		Address address = data.getAddress();
+		Element addressElement = doc.createElement("Address");
+		List<Long> activeShipments = data.getActiveShipment();
+		Element activeShipmentsElement = doc.createElement("ActiveShipments");
+		
 		newElementWithValue(newClient, "ClientID", String.valueOf(data.getClientID()));
 		newElementWithValue(newClient, "CompanyName", data.getCompanyName());
-		
-		Element phoneNumber = doc.createElement("PhoneNumber");
 		newElementWithValue(phoneNumber, "CountryCode", String.valueOf(data.getPhoneNumber().getCountryCode()));
 		newElementWithValue(phoneNumber, "PhoneNumberBase", String.valueOf(data.getPhoneNumber().getPhone()));
 		newClient.appendChild(phoneNumber);
-		
 		newElementWithValue(newClient, "Email", data.getEmail());
-		
-		ReferenceName name = data.getPerson();
-		Element nameElement = doc.createElement("RefrencePersonName");
+	
 		for(String firstname : name.getFirstName()) {
 			newElementWithValue(nameElement,"FirstName",firstname);
-		}
-		
+		}	
 		for(String middleName : name.getMiddleName()) {
 			newElementWithValue(nameElement,"MiddleName",middleName);
 		}
@@ -90,43 +89,43 @@ public class ClientAccess extends DataAccess<ClientData> {
 		}
 		newClient.appendChild(nameElement);
 		
-		Address address = data.getAddress();
-		Element addressElement = doc.createElement("Address");
 		newElementWithValue(addressElement,"StreetName",address.getStreetName());
 		newElementWithValue(addressElement,"HouseNumber",String.valueOf(address.getHouseNumber()));
 		newElementWithValue(addressElement,"City",address.getCity());
 		newElementWithValue(addressElement,"ZipCode",String.valueOf(address.getZipCode()));
 		newClient.appendChild(addressElement);
 		
-		Element activeShipments = doc.createElement("ActiveShipments");
-		for(long journeyID : data.getActiveShipment()) {
-			newElementWithValue(activeShipments,"JourneyID",String.valueOf(journeyID));
+		for(long journeyID : activeShipments) {
+			newElementWithValue(activeShipmentsElement,"JourneyID",String.valueOf(journeyID));
 		}
-		newClient.appendChild(activeShipments);
+		newClient.appendChild(activeShipmentsElement);
+		
 		return newClient;
 	}
 	
+	
 	protected ClientData dataFromElement(Element client) throws NumberFormatException, AmbiguousElementSelectionException, ElementNotFoundException {
-		long clientID = Integer.valueOf(nodeMethods.valueFromTagName(client, "ClientID"));
-		String companyName = nodeMethods.valueFromTagName(client, "CompanyName");
-		int countryCode = Integer.valueOf(nodeMethods.valueFromTagName(nodeMethods.singleElementFromTagName(client,"PhoneNumber"), "CountryCode"));
-		int phoneNumber = Integer.valueOf(nodeMethods.valueFromTagName(nodeMethods.singleElementFromTagName(client,"PhoneNumber"), "PhoneBaseNumber"));
-		String email = nodeMethods.valueFromTagName(client, "Email");
-		List<List<String>> names = getNames(client);
-		
 		Element addressElement = nodeMethods.singleElementFromTagName(client, "Address");
-		String streetName = nodeMethods.valueFromTagName(addressElement, "StreetName");
-		int houseNumber = Integer.valueOf(nodeMethods.valueFromTagName(addressElement, "HouseNumber"));
-		String city = nodeMethods.valueFromTagName(addressElement, "City");
-		String zipCode = nodeMethods.valueFromTagName(addressElement, "ZipCode");
-		
 		NodeList activeShipmentsElements = nodeMethods.singleElementFromTagName(client, "ActiveShipments").getChildNodes();
-		List<String> journeyIDStrings = getValuesFromChildNodes(activeShipmentsElements);
-		List<Long> journeyIDs = new ArrayList<>();
+		List<String> journeyIDStrings = nodeMethods.getValuesFromChildNodes(activeShipmentsElements);
+		
+		
+		long 			   clientID = Integer.valueOf(nodeMethods.valueFromTagName(client, "ClientID"));
+		String 			   companyName = nodeMethods.valueFromTagName(client, "CompanyName");
+		int 			   countryCode = Integer.valueOf(nodeMethods.valueFromTagName(nodeMethods.singleElementFromTagName(client,"PhoneNumber"), "CountryCode"));
+		int 			   phoneNumber = Integer.valueOf(nodeMethods.valueFromTagName(nodeMethods.singleElementFromTagName(client,"PhoneNumber"), "PhoneBaseNumber"));
+		String 			   email = nodeMethods.valueFromTagName(client, "Email");
+		List<List<String>> names = getNames(client);
+		String 			   streetName = nodeMethods.valueFromTagName(addressElement, "StreetName");
+		int 			   houseNumber = Integer.valueOf(nodeMethods.valueFromTagName(addressElement, "HouseNumber"));
+		String 			   city = nodeMethods.valueFromTagName(addressElement, "City");
+		String 			   zipCode = nodeMethods.valueFromTagName(addressElement, "ZipCode");
+		List<Long> 		   journeyIDs = new ArrayList<>();
+		
+		
 		for (int i = 0; i < journeyIDStrings.size(); i++) {
 			journeyIDs.add(Long.valueOf(journeyIDStrings.get(i)));
 		}
-		
 		
 		ClientData clientData = new ClientData(clientID, companyName, countryCode, phoneNumber, email, (ArrayList<String>) names.get(0), (ArrayList<String>) names.get(1), (ArrayList<String>) names.get(2), streetName, city, houseNumber, zipCode);
 		
@@ -134,7 +133,8 @@ public class ClientAccess extends DataAccess<ClientData> {
 		return clientData;
 	}
 
-	public List<List<String>> getNames(Element client) throws AmbiguousElementSelectionException, ElementNotFoundException {
+	
+	private List<List<String>> getNames(Element client) throws AmbiguousElementSelectionException, ElementNotFoundException {
 		List<List<String>> names = new ArrayList<List<String>>();
 		
 		Element namesElement = nodeMethods.singleElementFromTagName(client, "RefrencePersonName");
@@ -149,18 +149,10 @@ public class ClientAccess extends DataAccess<ClientData> {
 		
 		for(int i = 0; i < 3; i++) {
 			NodeList namesElements = namesElementsList.get(i);
-			names.add(getValuesFromChildNodes(namesElements));
+			names.add(nodeMethods.getValuesFromChildNodes(namesElements));
 		}
 		return names;
 	}
-	
-	public List<String> getValuesFromChildNodes(NodeList childNodes){
-		List<String> values = new ArrayList<String>();
-		int childNodesLen = childNodes.getLength();
-		for(int i = 0; i < childNodesLen; i++) {
-			values.add(childNodes.item(i).getTextContent());
-		}
-		return values;
-	}
+
 }
 
--- FinalProject/src/dataAccess/NodeMethods.java
+++ FinalProject/src/dataAccess/NodeMethods.java
@@ -1,6 +1,8 @@
 package dataAccess;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -31,5 +33,19 @@ public class NodeMethods {
 			throw new ElementNotFoundException("No element with that tag name");
 		}
 		return (Element) element.item(0);
+	}
+	
+	public List<String> getValuesFromChildNodes(NodeList childNodes){
+		List<String> values = new ArrayList<String>();
+		int childNodesLen = childNodes.getLength();
+		for(int i = 0; i < childNodesLen; i++) {
+			values.add(childNodes.item(i).getTextContent());
+		}
+		return values;
+	}
+	
+	public boolean needsToBeInsertedAtEnd(NodeList nodes, long newNodeID) {
+		int nodesLength = nodes.getLength();
+		return (nodesLength == 0) || (newNodeID >= getElementID((Element) nodes.item(nodesLength - 1)));
 	}
 }
