237cff6329a2e7c527fbacbcb927357451db0e01
Mamuna <muna.azam@hotmail.com>
2020-04-15 09:53:06 +0200

    some changes

--- FinalProject/test/simulation/SimulatingData.java
+++ FinalProject/test/simulation/SimulatingData.java
@@ -15,7 +15,7 @@ import objectsData.PortData;
 public class SimulatingData {
 
 	public static void main(String[] args) {
-	ClientAccess clientDataBase = new ClientAccess();
+	/*ClientAccess clientDataBase = new ClientAccess();
 	ArrayList<String> firstname = new ArrayList<String>();
 	ArrayList<String> middlename = new ArrayList<String>();
 	ArrayList<String> lastname = new ArrayList<String>();
@@ -28,7 +28,8 @@ public class SimulatingData {
 	ClientData client2 = new ClientData(36836570081685l,"company",92,23789,"email@eh.com",firstname,middlename,lastname,"g11/2","Islamabad",59,"2620");
 	clientDataBase.newEntry(client1);
 	clientDataBase.newEntry(client2);
-	clientDataBase.flushActiveData();
+	clientDataBase.flushActiveData();*/
+	
 	}
 	
 
