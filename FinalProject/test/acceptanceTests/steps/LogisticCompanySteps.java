05f362da9c1ee71f6a39026742ba4ff7f7f9f1f9
Katherine Fernandes <39100068+katherinefernandes@users.noreply.github.com>
2020-04-15 16:57:20 +0200

    cucumber tests for add client

--- FinalProject/test/acceptanceTests/steps/LogisticCompanySteps.java
+++ FinalProject/test/acceptanceTests/steps/LogisticCompanySteps.java
@@ -1,9 +1,10 @@
 package acceptanceTests.steps;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+import java.util.ArrayList;
 
 import dataAccess.ClientAccess;
 import exceptions.AmbiguousElementSelectionException;
@@ -12,13 +13,27 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import objectsData.ClientData;
-import users.LogisticCompany;
+import supportingClasses.ValidInput;
+import supportingClasses.parseInput;
+import users.LogisticCompanyV2;
 
 public class LogisticCompanySteps {
 	private ClientData client;
 	private ClientAccess clientDatabase = new ClientAccess();
-	private LogisticCompany logistic = new LogisticCompany();
-		
+	private LogisticCompanyV2 logistic = new LogisticCompanyV2();
+	private ValidInput validate = new ValidInput();
+	private String email;
+	private String name;
+	private int countryCode;
+	private long phone;
+	private ArrayList<String> firstName;
+	private ArrayList<String> middleName;
+	private ArrayList<String> lastName;
+	private String street;
+	private int houseNumber;
+	private String city;
+	private String postCode;
+	
 	@Given("the logistic Company decides to view the Client information")
 	public void the_logistic_Company_decides_to_view_the_Client_information(){
 	    // Write code here that turns the phrase above into concrete actions
@@ -57,7 +72,77 @@ public class LogisticCompanySteps {
 		assertThrows(NullPointerException.class,()->{client.getCompanyName();});
 	}
 
+	@Given("the logistic Company decides to add a new client")
+	public void theLogisticCompanyDecidesToAddANewClient() {
+	    // Write code here that turns the phrase above into concrete actions
+		assertFalse(logistic.getAddNewClient());
+		
+	    logistic.setAddNewClient();
+	    
+	    
+	}
 
-	
+	@When("the client email is {string}")
+	public void theClientEmailIs(String email) {
+	    // Write code here that turns the phrase above into concrete actions
+		assertTrue(validate.validateEmail(email));
+		
+		this.email = email;
+		
+		
+	}
+
+	@When("the client name is {string}")
+	public void theClientNameIs(String name) {
+	    // Write code here that turns the phrase above into concrete actions
+		assertTrue(validate.validateName(name));
+		
+		this.name = name;
+		
+	}
+
+	@When("the client phone number is {int} , {long}")
+	public void theClientPhoneNumberIs(int cc, long phone) {
+	    // Write code here that turns the phrase above into concrete actions
+		assertTrue("the country code should be valid",validate.validateCountryCode(cc));
+		assertTrue(validate.validatePhone(phone));
+		
+		this.countryCode = cc;
+		this.phone = phone;
+		
+	}
+
+	@When("the reference person is {string} {string} {string}")
+	public void theReferencePersonIs(String name1, String name2, String name3) {
+	    // Write code here that turns the phrase above into concrete actions
+	   assertTrue(validate.validateName(name1));
+	   assertTrue(validate.validateName(name2));
+	   assertTrue(validate.validateName(name3));
+	   
+	   this.firstName = parseInput.parsingNames(name1);
+	   this.middleName = parseInput.parsingNames(name2);
+	   this.lastName = parseInput.parsingNames(name3);
+	}
+
+	@When("the address is street: {string} house number: {int} city: {string} zipcode: {string}")
+	public void theAddressIsStreetHouseNumberCityZipcode(String street, int hNumber, String city, String zipcode) {
+	    // Write code here that turns the phrase above into concrete actions
+		assertTrue(validate.validateStreet(street));
+		assertTrue(validate.validateName(city));
+		assertTrue(validate.validatePostCode(zipcode));
+		
+		this.street = street;
+		this.houseNumber = hNumber;
+		this.city = city;
+		this.postCode = zipcode;
+	   
+	}
+
+	@Then("a unique client ID is generated")
+	public void aUniqueClientIDIsGenerated() {
+	    // Write code here that turns the phrase above into concrete actions
+		logistic.addClient(email, name, countryCode, phone, firstName, middleName, lastName, street, city, postCode, houseNumber);
+		assertTrue(logistic.getAddNewClient());
+	}
 }
 
