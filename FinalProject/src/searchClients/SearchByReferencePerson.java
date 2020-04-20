b661bf3c0f651ebcaf54b3dd600322373091bd16
Mamuna <muna.azam@hotmail.com>
2020-04-20 18:17:45 +0200

    all the features run now

--- FinalProject/src/searchClients/SearchByReferencePerson.java
+++ FinalProject/src/searchClients/SearchByReferencePerson.java
@@ -1,6 +1,9 @@
 package searchClients;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import dataAccess.ClientAccess;
 import objectsData.ClientData;
@@ -18,10 +21,39 @@ public class SearchByReferencePerson implements SearchClients{
 	
 	@Override
 	public List<ClientData> getClients() {
-		String fullName = referencePerson.getFirstName().toString() +
-				 referencePerson.getMiddleName().toString() +
-				 referencePerson.getLastName().toString();
-		return databaseClient.searchEntries(fullName);
+		String fullName="";
+		List<ClientData> firstClients;
+		List<ClientData> middleClients;
+		List<ClientData> lastClients;
+		for(int i=0;i<referencePerson.getFirstName().size();i++) {
+			fullName=fullName+referencePerson.getFirstName().get(i);
+		};
+		firstClients = databaseClient.searchEntries(fullName);
+		fullName="";
+		for(int i=0;i<referencePerson.getMiddleName().size();i++) {
+			fullName=fullName+referencePerson.getMiddleName().get(i);
+		};
+		middleClients=databaseClient.searchEntries(fullName);
+		fullName="";
+		for(int i=0;i<referencePerson.getLastName().size();i++) {
+			fullName=fullName+referencePerson.getLastName().get(i);
+		};
+		lastClients=databaseClient.searchEntries(fullName);
+		Set<ClientData> hSet = new HashSet<ClientData>(); 
+        for (ClientData x : firstClients ) {
+        	hSet.add(x);
+        }
+        for (ClientData x : middleClients ) {
+        	hSet.add(x);
+        }
+        for (ClientData x : lastClients ) {
+        	hSet.add(x);
+        }
+        List<ClientData> finalresult = new ArrayList<ClientData>();
+        for(ClientData x :hSet) {
+        	finalresult.add(x);
+        }
+		return finalresult;
 	}
 	
 }
