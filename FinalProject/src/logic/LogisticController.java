b9023d223c9024848ba84d960d46f5a0b24d9dd3
Mamuna <muna.azam@hotmail.com>
2020-04-21 13:21:55 +0200

    changes

---
+++ FinalProject/src/logic/LogisticController.java
@@ -0,0 +1,12 @@
+package logic;
+
+import users.LogisticCompanyV2;
+
+public class LogisticController {
+	private LogisticCompanyV2 logistic;
+	
+	
+	public LogisticController(){
+		logistic = new LogisticCompanyV2();
+	}
+}
