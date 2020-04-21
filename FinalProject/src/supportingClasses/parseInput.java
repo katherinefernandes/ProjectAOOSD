89287f0b9bc50561ce85c672d07449387e0c658a
Katherine Fernandes <39100068+katherinefernandes@users.noreply.github.com>
2020-04-21 03:11:37 +0200

    parseInput has getDate in the right format

--- FinalProject/src/supportingClasses/parseInput.java
+++ FinalProject/src/supportingClasses/parseInput.java
@@ -1,5 +1,7 @@
 package supportingClasses;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -11,4 +13,14 @@ public class parseInput {
 	/*public static void main(String[] args) {
 		System.out.println(parsingNames("munaazam"));
 	}*/ //mamuna-> I added this to test it out in console...
+	
+	
+	public static String getDate() {
+		LocalDateTime myDateObj = LocalDateTime.now();  
+	    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy");  
+	    
+	    String formattedDate = myDateObj.format(myFormatObj);  
+	    
+	    return formattedDate;
+	}
 }
